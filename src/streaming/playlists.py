622247a6da7d2cfdf443b73d3ade6d8cbc90f288
danielianevIMC <25imc10123@imc.ac.at>
2026-04-06 11:43:52 +0200

    updates

--- src/streaming/playlists.py
+++ src/streaming/playlists.py
@@ -7,3 +7,30 @@ Classes to implement:
   - Playlist
     - CollaborativePlaylist
 """
+class Playlist:
+    def __init__(self, playlist_id: str, title: str, owner):
+        self.playlist_id = playlist_id
+        self.title = title
+        self.owner = owner
+        self.tracks = []  
+
+    def add_track(self, track):
+        if track not in self.tracks:
+            self.tracks.append(track)
+
+    def remove_track(self, track):
+        if track in self.tracks:
+            self.tracks.remove(track)
+
+class CollaborativePlaylist(Playlist):
+    def __init__(self, playlist_id: str, title: str, owner):
+        super().__init__(playlist_id, title, owner)
+        self.contributors = [owner]
+
+    def add_contributor(self, user):
+        if user not in self.contributors:
+            self.contributors.append(user)
+
+    def remove_contributor(self, user):
+        if user in self.contributors and user != self.owner:
+            self.contributors.remove(user)
--- src/streaming/sessions.py
+++ src/streaming/sessions.py
@@ -6,3 +6,14 @@ Implement the ListeningSession class for recording listening events.
 Classes to implement:
   - ListeningSession
 """
+class ListeningSession:
+
+    def __init__(self, session_id: str, user, track, timestamp, duration_seconds: int):
+        self.session_id = session_id
+        self.user = user
+        self.track = track
+        self.timestamp = timestamp
+        self.duration_seconds = duration_seconds
+
+    def duration_listened_minutes(self) -> float:
+        return self.duration_seconds / 60.0
